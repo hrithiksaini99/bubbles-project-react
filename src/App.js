18e236ea721c53f34cafa73f7458389d959d592e
unknown <hrithiksaini99@gmail.com>
2022-12-20 03:37:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [bubbles, setBubbles] = useState([])
+  const [poppedBubbles, setPoppedBubbles] = useState([])
+  function addBubble(e) {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16)
+    setBubbles([
+      ...bubbles,
+      { x: e.clientX, y: e.clientY, color: `#${randomColor}` },
+    ])
+    console.log(bubbles)
+  }
+  function undoBubble() {
+    if (!bubbles.length) return
+    const newBubbles = [...bubbles]
+    const poppedBubble = newBubbles.pop()
+    setBubbles(newBubbles)
+    setPoppedBubbles([...poppedBubbles, poppedBubble])
+  }
+  function redoBubble() {
+    if (!poppedBubbles) return
+    setBubbles([...bubbles, poppedBubbles.pop()])
+  }
+  function clearBubble() {
+    setBubbles([])
+    setPoppedBubbles([])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="buttons">
+        <button
+          className="button"
+          disabled={!bubbles.length}
+          onClick={() => undoBubble()}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Undo
+        </button>
+        <button
+          className="button"
+          disabled={!poppedBubbles.length}
+          onClick={() => redoBubble()}
+        >
+          Redo
+        </button>
+        <button
+          className="button"
+          disabled={!poppedBubbles.length && !bubbles.length}
+          onClick={() => clearBubble()}
+        >
+          Clear
+        </button>
+      </div>
+      <div className="App" onClick={(e) => addBubble(e)}>
+        {bubbles?.map((bubble, _id) => {
+          return (
+            <div
+              key={_id}
+              className="bubble"
+              style={{
+                position: 'absolute',
+                top: bubble.y,
+                left: bubble.x,
+                backgroundColor: bubble.color,
+                marginLeft: -8,
+                marginTop: -8,
+              }}
+            ></div>
+          )
+        })}
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
